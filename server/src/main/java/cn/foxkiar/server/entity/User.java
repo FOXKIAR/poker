6e82c8379babc0333ebb92700b456846c1cb3f87
FOXKIAR <2399786141@qq.com>
2024-08-25 02:02:37 +0800

    添加一些注释

--- server/src/main/java/cn/foxkiar/server/entity/User.java
+++ server/src/main/java/cn/foxkiar/server/entity/User.java
@@ -15,20 +15,34 @@ public class User {
     private Session session;
     private LinkedList<Card> playerCards = new LinkedList<>();
 
-    public void discardCardsByEntity(List<Card> cards) {
-        for (Card card : cards) {
+    /**
+     * 用户的出牌行为
+     * @param cards 实体类列表：{@link Card}
+     */
+    public void discardCards(List<Card> cards) {
+        for (Card card : cards)
             playerCards.remove(card);
-        }
     }
 
+    /**
+     * 用户的抽牌行为
+     * @param cards 实体类列表：{@link Card}
+     */
     public void drawCards(List<Card> cards) {
         playerCards.addAll(cards);
     }
 
+    /**
+     * 用户的洗牌行为
+     */
     public void sortPlayerCards() {
         playerCards.sort(Card::compareTo);
     }
 
+    /**
+     * 用于判断是否获胜
+     * @return {@link Boolean}
+     */
     public boolean isWinVictory() {
         return !playerCards.isEmpty();
     }
